fc8ad0e1c11c1aeac542dd5f3275d4b815e498d0
sergiogomes <sergiopgomes92@gmail.com>
2020-11-29 19:53:44 -0300

    Creating searching text slice.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
 import counterReducer from '../features/counter/counterSlice';
+import searchTextReducer from '../core/components/header/headerSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    searchText: searchTextReducer,
   },
 });
 
---
+++ src/core/components/header/headerSlice.ts
@@ -0,0 +1,37 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+import { RootState } from '../../../app/store';
+
+interface SearchTextState {
+  value: string;
+}
+
+const initialState: SearchTextState = {
+  value: '',
+};
+
+export const searchTextSlice = createSlice({
+  name: 'searchText',
+  initialState,
+  reducers: {
+    changeText: (state, action: PayloadAction<string>) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { changeText } = searchTextSlice.actions;
+
+export const selectSearchedText = (state: RootState) => state.searchText;
+
+export default searchTextSlice.reducer;
+
+
+
+
+
+
+
+
+
+
