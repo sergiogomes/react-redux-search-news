c1063b3306cebd35f1b5eca9ea6799a28f474fe9
sergiogomes <sergiopgomes92@gmail.com>
2020-12-01 21:57:49 -0300

    Rendirecting between Home and Search pages.

--- src/core/components/header/Header.tsx
+++ src/core/components/header/Header.tsx
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Button, Form, FormControl } from 'react-bootstrap';
 import Navbar from 'react-bootstrap/Navbar'
+import { Link, Redirect } from 'react-router-dom';
 
 import { selectSearchedText, changeText } from './headerSlice';
 // import styles from './Header.module.css';
 
 const Header = () => {
+  const [isSearch, setSearch] = useState(false);
   const searchText = useSelector(selectSearchedText);
   const dispatch = useDispatch();
 
   const handleClick = () => {
-    console.log('clicked.');
+    setSearch(true);
+    setTimeout(() => setSearch(false), 0);
+  };
+
+  const renderRedirect = () => {
+    if (isSearch) {
+      return <Redirect to={`/search?q=${searchText}`} />;
+    }
   };
 
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">React News</Navbar.Brand>
+      <Navbar.Brand>
+        <Link to="/">React News</Link>
+      </Navbar.Brand>
       <Form inline>
         <FormControl type="text" value={searchText}
           placeholder="Search" className="mr-sm-2"
@@ -24,6 +35,7 @@ const Header = () => {
         <Button variant="outline-success"
           onClick={handleClick}>Search</Button>
       </Form>
+      {renderRedirect()}
     </Navbar>
   );
 }
