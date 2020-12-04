c886bd9ec981e27d9d6c47266c5ed7a1f68f3f3d
sergiogomes <sergiopgomes92@gmail.com>
2020-12-04 20:53:46 -0300

    Filling the home page.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { Jumbotron } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div>Home works!</div>
+    <Jumbotron>
+      <h1 className="display-4">Home Page</h1>
+      <p className="lead">Get breaking news headlines, and search for articles from news sources and blogs all over the web with our news API</p>
+      <hr className="my-4" />
+      <p>React Search News is a simple and easy-to-use app that consumes News API for headlines and articles live all over the web right now.</p>
+    </Jumbotron>
   )
 }
 
