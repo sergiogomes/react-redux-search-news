9aa45407ad09e6f4d7f78a9cf5f957581c674ec9
sergiogomes <sergiopgomes92@gmail.com>
2020-12-14 22:35:45 -0300

    Creating tests for Alert Message component.

---
+++ src/core/components/alert-message/AlertMessage.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+
+import AlertMessage from './AlertMessage';
+
+const show = true;
+const title = 'Alert!';
+const variant = 'info';
+const message = 'Testing alert message.';
+const actionTitle = 'Click me!';
+const handleAction = () => console.log('Button clicked!');
+
+let wrapped: ReactWrapper;
+
+beforeEach(() => {
+  wrapped = mount(
+    <AlertMessage
+      variant={variant}
+      show={show}
+      message={message}
+      title={title}
+      action={handleAction}
+      actionTitle={actionTitle}
+    />
+  );
+});
+
+it('should contain a alert message with a title Alert!', () => {
+  expect(wrapped.render().text()).toContain(title);
+});
+
+it('should contain a alert message with a message Testing alert message.', () => {
+  expect(wrapped.render().text()).toContain(message);
+});
+
+it('should contain a alert message with a button Click me!', () => {
+  expect(wrapped.render().text()).toContain(actionTitle);
+});
