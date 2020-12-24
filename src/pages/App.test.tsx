576409ef0a734b85e38bce0ee9e08509a44c7c1a
sergiogomes <sergiopgomes92@gmail.com>
2020-12-24 18:53:59 -0300

    Addind shallow test to the App component.

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
+import { mount, ReactWrapper, shallow, ShallowWrapper } from 'enzyme';
 
 import App from './App';
 import ReduxProvider from '../reduxProvider';
 
-let wrapped: ReactWrapper;
+describe('Mounted App component', () => {
+  let wrapped: ReactWrapper;
 
-beforeEach(() => {
-  wrapped = mount(
-    <ReduxProvider>
-      <App />
-    </ReduxProvider>,
-  );
+  beforeEach(() => {
+    wrapped = mount(
+      <ReduxProvider>
+        <App />
+      </ReduxProvider>,
+    );
+  });
+
+  it('should contain the title React News', () => {
+    const title = 'React News';
+    expect(wrapped.render().text()).toContain(title);
+  });
 });
 
-it('should contain the title React News', () => {
-  const title = 'React News';
-  expect(wrapped.render().text()).toContain(title);
+describe('Shallowed App component', () => {
+  let wrapped: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapped = shallow(<App />);
+  });
+
+  it('should contain an App class name', () => {
+    expect(wrapped.find('.App').exists()).toBe(true);
+  });
 });
