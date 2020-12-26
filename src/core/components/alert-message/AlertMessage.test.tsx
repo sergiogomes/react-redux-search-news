e71cbf61716083aba89a54a2c43a9f0558becbea
sergiogomes <sergiopgomes92@gmail.com>
2020-12-25 23:22:30 -0300

    Adding a describe block to the alert message component test.

--- src/core/components/alert-message/AlertMessage.test.tsx
+++ src/core/components/alert-message/AlertMessage.test.tsx
@@ -10,29 +10,32 @@ const message = 'Testing alert message.';
 const actionTitle = 'Click me!';
 const handleAction = () => console.log('Button clicked!');
 
-let wrapped: ReactWrapper;
+describe('Mounted Alert Message component', () => {
+  let wrapped: ReactWrapper;
 
-beforeEach(() => {
-  wrapped = mount(
-    <AlertMessage
-      variant={variant}
-      show={show}
-      message={message}
-      title={title}
-      action={handleAction}
-      actionTitle={actionTitle}
-    />
-  );
-});
+  beforeEach(() => {
+    wrapped = mount(
+      <AlertMessage
+        variant={variant}
+        show={show}
+        message={message}
+        title={title}
+        action={handleAction}
+        actionTitle={actionTitle}
+      />
+    );
+  });
 
-it('should contain a alert message with a title Alert!', () => {
-  expect(wrapped.render().text()).toContain(title);
-});
+  it('should contain a alert message with a title Alert!', () => {
+    expect(wrapped.render().text()).toContain(title);
+  });
 
-it('should contain a alert message with a message Testing alert message.', () => {
-  expect(wrapped.render().text()).toContain(message);
-});
+  it('should contain a alert message with a message Testing alert message.', () => {
+    expect(wrapped.render().text()).toContain(message);
+  });
 
-it('should contain a alert message with a button Click me!', () => {
-  expect(wrapped.render().text()).toContain(actionTitle);
+  it('should contain a alert message with a button Click me!', () => {
+    expect(wrapped.render().text()).toContain(actionTitle);
+  });
+
 });
