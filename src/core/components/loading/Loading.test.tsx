2ee119497453e8c47f8a8d95e5e7c3365565eef1
sergiogomes <sergiopgomes92@gmail.com>
2020-12-27 23:28:34 -0300

    Adding one shallowed test for Loading component.

--- src/core/components/loading/Loading.test.tsx
+++ src/core/components/loading/Loading.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
+import { mount, ReactWrapper, shallow, ShallowWrapper } from 'enzyme';
 
 import Loading from './Loading';
 import ReduxProvider from '../../../reduxProvider';
@@ -18,6 +18,24 @@ describe('Mounted Loading component', () => {
   it('should contain the text wait a moment', () => {
     const text = 'Wait just a moment...';
     expect(wrapped.render().text()).toContain(text);
+  });
+
+  afterAll(() => wrapped.unmount());
+});
+
+describe('Shallowed Loading component', () => {
+  let wrapped: ShallowWrapper;
+
+  beforeEach(() => {
+    wrapped = shallow(
+      <ReduxProvider>
+        <Loading />
+      </ReduxProvider>
+    );
+  });
+
+  it('should contain a Loading component', () => {
+    expect(wrapped.find(Loading)).toHaveLength(1);
   });
 
   afterAll(() => wrapped.unmount());
